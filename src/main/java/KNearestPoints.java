966de41492d992e5b0df05b7d409314c88d1c806
lonaeyeo <lonaeyeo@163.com>
2020-11-09 22:06:10 +0800

    fip - update

---
+++ src/main/java/KNearestPoints.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class KNearestPoints {
+
+    @Test
+    public void test() {
+//        ArrayList<Integer> al = new ArrayList<>();
+        System.out.println(Math.pow(4, 2));
+        System.out.println(Math.sqrt(4));
+    }
+
+    public int[][] kClosest(int[][] points, int K) {
+        return null;
+    }
+}
