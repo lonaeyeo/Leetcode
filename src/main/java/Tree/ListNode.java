ce8f046411e2b645f4602d7c9b8dcfca9595fdd4
lonaeyeo <lonaeyeo@163.com>
2020-11-21 13:21:02 +0800

    lonaeyeo laptop commit

--- src/main/java/Tree/ListNode.java
+++ src/main/java/Tree/ListNode.java
@@ -7,14 +7,40 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
     public ListNode buildList(int[] vals) {
         ListNode pre = new ListNode(vals[0]);
         ListNode head = pre;
-        for (int i = 1; i < vals.length; ++i){
+        for (int i = 1; i < vals.length; ++i) {
             ListNode listNode = new ListNode(vals[i]);
             pre.next = listNode;
             pre = listNode;
         }
         return head;
+    }
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode atomicHead = new ListNode(0);
+        atomicHead.next = head;
+        ListNode curr = head.next;
+        ListNode lastSorted = head;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = curr;
+                curr = curr.next;
+            } else {
+                ListNode prev = atomicHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+                curr = lastSorted.next;
+            }
+        }
+        return atomicHead.next;
     }
 }
