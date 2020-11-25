ed131c1b49daf9c756b9155977ce6f8a2cc63b77
lonaeyeo <lonaeyeo@163.com>
2020-11-25 23:32:01 +0800

    lonaeyeo laptop commit

--- src/main/java/Array/ArrayOperation.java
+++ src/main/java/Array/ArrayOperation.java
@@ -86,11 +86,44 @@ public class ArrayOperation {
             count2[flag]++;
         }
 
-        for (int j = 0; j <26; j++) {
+        for (int j = 0; j < 26; j++) {
             if (count1[j] != count2[j])
                 return false;
         }
 
         return true;
+    }
+
+    @Test
+    public void test2() {
+        System.out.println(ArrayOperation.sortString("leetcode"));
+
+    }
+
+    public static String sortString(String s) {
+        // 用26位数组来装
+        int[] nums = new int[26];
+
+        for (int i = 0; i < s.length(); ++i) {
+            ++nums[s.charAt(i) - 'a'];
+        }
+
+        StringBuffer ans = new StringBuffer();
+
+        while (ans.length() < s.length()) {
+            for (int i = 0; i < 26; ++i) {
+                if (nums[i] != 0) {
+                    --nums[i];
+                    ans.append((char) (i + 'a'));
+                }
+            }
+            for (int i = 25; i >= 0; --i) {
+                if (nums[i] != 0) {
+                    --nums[i];
+                    ans.append((char) (i + 'a'));
+                }
+            }
+        }
+        return ans.toString();
     }
 }
