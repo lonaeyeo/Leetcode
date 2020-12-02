6f2d8a34aed210f565c6f898dec0ecaadef3d10b
lonaeyeo <lonaeyeo@163.com>
2020-12-02 20:05:05 +0800

    fip laptop update

--- src/main/java/ArrayAndSort/ToughClassP1.java
+++ src/main/java/ArrayAndSort/ToughClassP1.java
@@ -1,6 +1,5 @@
 package ArrayAndSort;
 
-import com.sun.xml.internal.org.jvnet.mimepull.CleanUpExecutorFactory;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,6 +9,10 @@ public class ToughClassP1 {
     @Test
     public void test() {
         ToughClassP1 toughClassP1 = new ToughClassP1();
+        int[] ans = toughClassP1.maxNumber(new int[]{8, 6, 9}, new int[]{1, 7, 5}, 3);
+        for (int a : ans) {
+            System.out.println(a);
+        }
 //        System.out.println(ToughClassP1.reversePairs(new int[]{1, 3, 2, 3, 1}));
 //        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE >> 1));
 //        System.out.println(Integer.toBinaryString(Integer.MAX_VALUE));
@@ -58,5 +61,54 @@ public class ToughClassP1 {
         return ans;
     }
 
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int s1Limit = nums1.length;
+        int s2Limit = nums2.length;
+        int s1Curr = 0;
+        int s2Curr = 0;
+        int max1 = s1Curr;
+        int max2 = s2Curr;
+
+        int[] ans = new int[k];
+        int index = 0;
+        int leftK = k;
+
+        while (leftK > 0) {
+            max1 = s1Curr;
+            max2 = s2Curr;
+            int forwardBoundary = nums1.length - s1Curr + nums2.length - s2Curr - leftK;
+            s1Limit = s1Curr + forwardBoundary < nums1.length - 1 ? s1Curr + forwardBoundary : nums1.length - 1;
+            s2Limit = s2Curr + forwardBoundary < nums2.length - 1 ? s2Curr + forwardBoundary : nums2.length - 1;
+
+            for (int i = s1Curr; i <= s1Limit; ++i) {
+                if (nums1[i] > nums1[max1])
+                    max1 = i;
+            }
+            for (int i = s2Curr; i <= s2Limit; ++i) {
+                if (nums2[i] > nums2[max2])
+                    max2 = i;
+            }
+
+            if (s1Curr == nums1.length) {
+                ans[index] = nums2[max2];
+                s2Curr = max2 + 1;
+            } else if (s2Curr == nums2.length) {
+                ans[index] = nums1[max1];
+                s1Curr = max1 + 1;
+            } else {
+                if (nums1[max1] >= nums2[max2]) {
+                    ans[index] = nums1[max1];
+                    s1Curr = max1 + 1;
+                } else {
+                    ans[index] = nums2[max2];
+                    s2Curr = max2 + 1;
+                }
+            }
+
+            ++index;
+            --leftK;
+        }
+        return ans;
+    }
 
 }
