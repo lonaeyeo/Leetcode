e9dae704d29eaa552c05b1de00d71ef90de0ddbb
lonaeyeo <lonaeyeo@163.com>
2020-12-05 17:58:46 +0800

    fip laptop update

--- src/main/java/Tree/BinaryTreeOperation.java
+++ src/main/java/Tree/BinaryTreeOperation.java
@@ -82,4 +82,14 @@ public class BinaryTreeOperation {
 
         return allTrees;
     }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        else if (p != null && q != null)
+            if (p.val == q.val)
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            else return false;
+        else return false;
+    }
 }
