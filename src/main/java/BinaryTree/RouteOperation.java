b71cdc998bd4f1a628383c827b3eb37b5d78eac3
lonaeyeo <lonaeyeo@163.com>
2020-12-19 16:36:07 +0800

    fip laptop update

--- src/main/java/BinaryTree/RouteOperation.java
+++ src/main/java/BinaryTree/RouteOperation.java
@@ -1,5 +1,6 @@
 package BinaryTree;
 
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,5 +21,31 @@ public class RouteOperation {
         else
             return sumNumbers(root.left, currSum) + sumNumbers(root.right, currSum);
     }
+
+    // 任意一点到任意点最大路径
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null)
+            return 0;
+        maxBranch(root);
+        return maxSum;
+    }
+
+    // 计算当前节点的左右子树中的最大分枝和，例如root=1，left=10，right=1，那么最大分枝和就是1+10=11
+    private int maxBranch(TreeNode root) {
+        if (root == null)
+            return 0;
+        int leftBranch = Math.max(maxBranch(root.left), 0);
+        int rightBranch = Math.max(maxBranch(root.right), 0);
+
+        int currSum = 0;
+        // 计算当前节点的最大路径，并与全局maxSum比较，已去除branch小于0的情况
+        currSum = root.val + leftBranch + rightBranch;
+        maxSum = Math.max(currSum, maxSum);
+
+        return root.val + Math.max(leftBranch, rightBranch);
+    }
+
 
 }
