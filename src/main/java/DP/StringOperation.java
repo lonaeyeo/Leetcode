13035a4b2e2f938a7a15d790af66f043ae76e2ed
lonaeyeo <lonaeyeo@163.com>
2021-02-06 00:30:05 +0800

    lonaeyeo laptop commit

--- src/main/java/DP/StringOperation.java
+++ src/main/java/DP/StringOperation.java
@@ -39,4 +39,30 @@ public class StringOperation {
         }
         return ans;
     }
+
+    /**
+     * 最长公共子序列
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        // base case：一个字符串为空，那么最长公共子序列长度为0
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    // dp[i-1][j-1]必定是<=dp[i][j-1]或者dp[i-1][j]
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    private int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
 }
