5a56ed39cc7f4002b91ccde13706405dfd54cb20
lonaeyeo <lonaeyeo@163.com>
2021-02-05 20:40:49 +0800

    lonaeyeo laptop commit

--- src/main/java/DP/ArrayOperation.java
+++ src/main/java/DP/ArrayOperation.java
@@ -1,5 +1,8 @@
 package DP;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArrayOperation {
 
     /**
@@ -21,5 +24,31 @@ public class ArrayOperation {
             maxLen = Math.max(len, maxLen);
         }
         return maxLen;
+    }
+
+    /**
+     * 最长续增子序列（不连续）
+     */
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (nums.length == 0) return 0;
+        // 以[i]结尾的最长递增子序列
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        int ans = 1;
+
+        for (int i = 1; i < len; i++) {
+            // 暴力
+            for (int j = 0; j < i; j++) {
+                // 扫描之前每一个dp，发现比自己小的就+1，并和自己比较大
+                // 所以一路都会是“当前最大”的状态
+                if (nums[i] > nums[j])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
     }
 }
