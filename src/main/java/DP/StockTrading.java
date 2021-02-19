024b3330a501c8468fa82277aeae4410367fd759
lonaeyeo <lonaeyeo@163.com>
2021-02-20 00:14:38 +0800

    lonaeyeo laptop commit

--- src/main/java/DP/StockTrading.java
+++ src/main/java/DP/StockTrading.java
@@ -17,4 +17,29 @@ public class StockTrading {
 
         return max;
     }
+
+
+    /**
+     * 可进行最多两次 获利操作
+     * 新的买入之前，旧的必须卖出
+     */
+    public int maxProfit2(int[] prices) {
+        int m = prices.length;
+        // 第一次买或者没有操作
+        int buy1 = -prices[0];
+        // 第二次买或者没有操作
+        int buy2 = -prices[0];
+        int sell1 = 0;
+        int sell2 = 0;
+
+        for (int i = 1; i < m; i++) {
+            // 逢低买进
+            buy1 = Math.max(buy1, -prices[i]);
+            // 计算逢高卖出
+            sell1 = Math.max(sell1, prices[i] + buy1);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
+        }
+        return sell2;
+    }
 }
